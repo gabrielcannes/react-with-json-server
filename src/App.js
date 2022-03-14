25b01e87923c4ffe7ee9d71ad38d27c4397857db
gabrielcannes <gabrielcannes.gc@gmail.com>
2022-03-14 18:39:53 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,76 @@
+// https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=5764s&ab_channel=TraversyMedia
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTasks from "./components/AddTasks";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const App = () => {
 
-    const [showAddTask, setShowAddTask] = useState (false)
+    const [showAddTask, setShowAddTask] = useState(false)
 
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Doctors Appointment',
-            day: 'Feb 5th at 2:30pm',
-            reminder: true
-        }, {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 1:30pm',
-            reminder: true
-        }, {
-            id: 3,
-            text: 'Food Shopping',
-            day: 'Feb 5th at 2:30pm',
-            reminder: false
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
         }
-    ])
+        getTasks()
+    }, [])
 
-    const addTask = (task) => {
-        const id = Math.floor(Math.random()* 10000)+1
-        const newTask = {id, ...task}
-        setTasks([...tasks, newTask])
+    const fetchTasks = async () => {
+        const response = await fetch('http://localhost:5000/tasks')
+        const data = await response.json()
+
+        return data
     }
 
-    const deleteTask = (id) => {
+    const fetchTask = async (id) => {
+        const response = await fetch(`http://localhost:5000/tasks/${id}`)
+        const data = await response.json()
+
+        return data
+    }
+
+    const addTask = async (task) => {
+        const  response = await fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+
+        const data = await response.json()
+
+        setTasks([...tasks, data])
+    }
+
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
-    const toggleReminder = (id) => {
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+        const response = await fetch (`http://localhost:5000/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+
+        const data = await response.json()
+
         setTasks(tasks.map(
             (task) => task.id === id
                 ? {
                     ...task,
-                    reminder: !task.reminder
+                    reminder: data.reminder
                 }
                 : task
         ))
@@ -49,7 +78,7 @@ const App = () => {
 
     return (
         <div className="container">
-            <Header title={'Task Tracker'}></Header>
+            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}></Header>
             {showAddTask && <AddTasks onAdd={addTask}></AddTasks>}
             {
                 tasks.length > 0
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import Button from './Button'
 
-const Header = ({title}) => {
-    const onClick = () => {
-        console.log('click')
-    }
+const Header = ({title, onAdd, showAdd}) => {
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick}></Button>
+            <Button
+                color={showAdd
+                    ? 'red'
+                    : 'green'}
+                text={showAdd
+                    ? 'Close'
+                    : 'Add'}
+                onClick={onAdd}></Button>
         </header>
     )
 }
